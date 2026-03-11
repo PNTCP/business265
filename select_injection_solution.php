34177a37b6c5e8e1807fab5939f63e3ffb7b8950
PNTCP <nattaponchanprom@gmail.com>
2026-03-11 11:46:52 +0700

    super last

--- select_injection_solution.php
+++ select_injection_solution.php
@@ -2,15 +2,69 @@
     require 'connect.php';
 
     $cid = $_GET["CustomerID"];
-    $sql = "SELECT * FROM customer where CustomerID = :customerID";
+    $sql = "SELECT * FROM customer c INNER JOIN country co
+        ON c.CountryCode = co.CountryCode where CustomerID = :customerID";
     $stmt = $conn->prepare($sql);
 
     $stmt->bindParam(':customerID', $cid);
     $stmt->execute();
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    while ($row = $stmt->fetch()){
-        echo $row['CustomerID'] . '' . $row['Name'] . "<br/>";
-    }
+    // while ($row = $stmt->fetch()){
+    //     echo $row['CustomerID'] . $row['Name'] . $row['OutstandingDebt'] 
+    //     . $row['CountryName'] . $row['Email'] ."<br/>";
+    // }
 
     $conn = null;
 ?>
+      <table width="800" border="1" style="border:blue">
+          <tr class = "headTr">
+              <th width="90">
+                  <div align="center">รหัสผู้ใช้ </div>
+              </th>
+              <th width="120">
+                  <div align="center">ชื่อ </div>
+              </th>
+                <th width="50">
+                  <div align="center">ยอดหนี้ </div>
+              </th>
+              <th width="100">
+                  <div align="center">ประเทศ </div>
+              </th>
+
+              <th width="70">
+                  <div align="center">อีเมล์</div>
+              </th>
+          </tr>
+
+          <?php
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                while ($row = $stmt->fetch()){
+            ?>
+                    <tr class = "columnTr">
+                  <td>
+                     <a href="detail.php?CustomerID=<?php echo $row['CustomerID'];?>">
+                        <?php echo $row['CustomerID']?>
+                     </a>
+                  </td>
+
+                  <td>
+                      <?php echo $row["Name"]; ?>
+                  </td>
+
+                    <td>
+                        <?php echo $row["OutstandingDebt"]; ?>
+                    </td>
+
+                  <td>
+                        <?php echo $row["CountryName"]?>
+                    </td>
+
+                  <td>
+                        <?php echo $row["Email"]  ?>
+                    </td>
+              </tr>
+    <?php
+        }
+    ?>
+          
+    </table>
